3c29644ff4b683798950f39bb6fcba55b05afef9
er-prashant-in <er.prashant.in@gmail.com>
2023-03-28 00:01:54 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT="-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirement
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        return requirements
+
+
+
+
+setup(
+    name='ml_project_01',
+    version='0.0.1',
+    author='PRASHANT SHARMA',
+    author_email='er.prashant.in@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirement.txt')
+)
